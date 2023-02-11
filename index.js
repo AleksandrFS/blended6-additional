589c09fec5da4bf48f9b8f1af9d85b856b9e5b40
AleksandrFedorov <ol.m.fed@gmail.com>
2023-02-11 14:58:22 +0200

    repoInit

---
+++ index.js
@@ -0,0 +1,38 @@
+// 1. Використовуючи функцію if...else,
+// напишіть код, який запитуватиме:
+// "Яка офіційна назва JavaScript?"
+// Якщо користувач вводить "ECMAScript",
+// то показати через alert: "Вірно!"
+// інакше відобразити:"Не знаєте? ECMAScript!"
+
+// const officialName = prompt("Яка офіційна назва JavaScript?");
+// if (officialName === "ECMAScript") {
+//   alert("Вірно!");
+// } else {
+//   alert("Не знаєте? ECMAScript!");
+// }
+
+
+// 2.  Напишіть цикл, який виводить у консоль числа від max до min за спаданням
+//  Виведіть у консоль  усіх парних чисел від min до max
+
+// const max = 50
+// const min = 23
+
+// for (let i = max; i >= min; i -= 1) {
+//     console.log(i);
+// }
+
+
+// 3. За допомогою циклу for додайте всі парні числа від min до max
+
+// const max = 50;
+// const min = 23;
+// let total = 0;
+
+// for (let i = max; i >= min; i -= 1) {
+//     if (i % 2 === 0) {
+//         total += i;
+//   }
+// }
+//   console.log(total);
